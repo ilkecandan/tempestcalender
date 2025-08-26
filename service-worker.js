a7bff3efa590f283493172f9cdb3a1cfa5dfe24e
İlke Candan Bengi <64421962+ilkecandan@users.noreply.github.com>
2025-08-26 15:29:58 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,7 @@
-const CACHE_NAME = 'tempest-content-calendar-v3';
+const CACHE_NAME = 'tempest-content-calendar-v4';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
